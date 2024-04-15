7924a025c2672deb6d8a14e1dceefe76f01ccd98
Rohan Truong <trongit2904@gmail.com>
2024-04-15 22:39:46 +0700

    add hpa

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5eed3601c68f4d978156e1d634bfea4-698127396.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
